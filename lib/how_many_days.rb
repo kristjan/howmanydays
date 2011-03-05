abd72c57fb881f671dc814d214d9a6e600cef652
Kristján Pétursson <kristjan@gmail.com>
2011-03-05 01:01:02 -0800

    Select both start and end date

--- lib/how_many_days.rb
+++ lib/how_many_days.rb
@@ -4,11 +4,13 @@ require 'haml'
 class HowManyDays < Sinatra::Base
     get '/' do
       begin
-        @date = Date.parse(params[:date]) if params[:date]
+        @start_date = Date.parse(params[:start_date]) if params[:start_date]
+        @end_date   = Date.parse(params[:end_date]) if params[:end_date]
       rescue => ex
-        @msg = "#{ex.message.capitalize}: #{params[:date]}"
+        @msg = "#{ex.message.capitalize}: #{params[:start_date]}, #{params[:end_date]}"
       end
-      @days = (@date - Date.today) if @date
+      @start_date ||= Date.today
+      @days = (@end_date - @start_date) if @start_date && @end_date
       haml :index
     end
 end

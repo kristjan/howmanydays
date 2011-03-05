7f2860d1f7a64751af42c1cea6ac832aa3c1d918
Kristján Pétursson <kristjan@gmail.com>
2011-03-05 00:55:30 -0800

    Catch bad dates

--- lib/how_many_days.rb
+++ lib/how_many_days.rb
@@ -3,7 +3,11 @@ require 'haml'
 
 class HowManyDays < Sinatra::Base
     get '/' do
-      @date = Date.parse(params[:date]) if params[:date]
+      begin
+        @date = Date.parse(params[:date]) if params[:date]
+      rescue => ex
+        @msg = "#{ex.message.capitalize}: #{params[:date]}"
+      end
       @days = (@date - Date.today) if @date
       haml :index
     end
